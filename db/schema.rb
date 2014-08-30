dc2fdd904a51559de94c812dc8961de9fe188373
Oana Sipos <oanasipos@gmail.com>
2014-08-30 07:53:04 +0200

    Added links to navbar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140822153917) do
+ActiveRecord::Schema.define(version: 20140829223833) do
 
   create_table "books", force: true do |t|
     t.string   "title"
@@ -39,6 +39,11 @@ ActiveRecord::Schema.define(version: 20140822153917) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.date     "reminder_date"
+  end
+
+  create_table "places", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "presents", force: true do |t|
